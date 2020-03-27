7ef1568ef42a46a67fa635a29b732a6cd3442e12
Sunil sharma <sharma.sunil8879@gmail.com>
2020-03-27 19:22:37 +0530

    all changes done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,30 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <title>Corona Virus Api</title>       
     </head>
     <body>
         <div class="flex-center position-ref full-height">            
 
             <div class="content">
-                <div class="title m-b-md">
-                    Information
+                <div class="container text-center">
+                    <h2>India Coronavirus Information</h2>
                 </div>
 
-                <div class="links">
+                <div class="container">
                     <ul>
                         @foreach($data as $key=>$value)
-                            <li>{{ $key }} :
+                            <li>{{ ucfirst($key) }} :
                                 @if(is_array($value))   
                                     
                                     {{ $value['value'] }}
                                     
                                 @else
-                                {{ $value }}    
+                                {{ date ( "Y-m-d H:i:s" , strtotime ( $value )) }}    
                                 @endif   
                             </li>
                         @endforeach
